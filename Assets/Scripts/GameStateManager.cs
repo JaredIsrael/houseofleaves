473d41c31cab1d8589d4514fb6d72f0cee935388
Cadecicc <cadeciccone@gmail.com>
2023-01-24 21:08:23 -0500

    Pausing and Resetting
    I implemented pausing (done by the esc key) and resetting (done by the R key)

---
+++ Assets/Scripts/GameStateManager.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameStateManager : MonoBehaviour
+{
+    [SerializeField] GameObject pauseScreen;
+    private int pauseHits;
+
+    void Start()
+    {
+        pauseScreen.SetActive(false);
+    }
+
+    void Update()
+    {
+        
+    }
+
+    public void ReadPauseInput(bool paused)
+    {
+        if (paused)
+        {
+            pauseHits++;
+        }
+        if (pauseHits % 2 == 1)
+        {
+            Time.timeScale = 0;
+            pauseScreen.SetActive(true);
+        } else
+        {
+            if (Time.timeScale != 1)
+            {
+                Time.timeScale = 1;
+            }
+            if (pauseScreen.activeInHierarchy == true)
+            {
+                pauseScreen.SetActive(false);
+            }
+        }
+    }
+
+    public void ReadResetInput(bool reset)
+    {
+        if (reset)
+        {
+            SceneManager.UnloadSceneAsync("PlayTestArea");
+            SceneManager.LoadSceneAsync("PlayTestArea");
+        }
+            
+    }
+}
