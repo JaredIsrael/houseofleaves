c146adb0459f7d9b02fba167d784c136b49f3a0e
Jared Israel <jared.seattle@gmail.com>
2023-01-25 22:24:23 -0500

    Added menu scene, manor assets, and project reorg

---
+++ Assets/Scripts/StartText.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StartText : MonoBehaviour
+{
+    public void LoadStartGame()
+    {
+        // Not currenlty using loading canvas, too fast!
+        LoadManager.Instance.LoadSceneBackground("PlayTestArea");
+    }
+}
