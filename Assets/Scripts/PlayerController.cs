2235e527ad847b0bf8fd65cff7f8287b2b720eda
Jared Israel <jared.seattle@gmail.com>
2023-02-05 21:15:24 -0500

    Added camera and movmement toggle methods in PlayerController for puzzle dev

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,7 +5,8 @@ using UnityEngine.InputSystem;
 
 /*
 
-Purpose: This class handles player movement for given input
+Purpose: This class handles player movement for controller input, disabling movement,
+and camera management
 
 Author: 
  
@@ -13,8 +14,9 @@ Author:
 
 public class PlayerController : MonoBehaviour
 {
-    [SerializeField] CharacterController controller;
-    [SerializeField] Camera cam;
+    public CharacterController controller;
+    public Camera cam;
+    private bool canMove = true;
 
     public float speed = 0;
 
@@ -23,12 +25,44 @@ public class PlayerController : MonoBehaviour
     void Update()
     {
         Vector3 movement3D = (transform.right * keyboardInput.x + transform.forward * keyboardInput.y) * speed;
-
+        if (!canMove)
+        {
+            movement3D = Vector3.zero;
+        }
         controller.Move(movement3D * Time.deltaTime);
+
     }
 
     public void ReadInput(Vector2 input)
     {
         keyboardInput = input;
     }
+
+    /*
+     
+    These could be done with a toggle method or directly modifying instance vars, but
+    this might reduce bugs and make any additional things we want to do on movmenet
+    disable easier (UI icon?) Can easily be reworked later if need be
+
+     */
+    public void EnableMovement()
+    {
+        canMove = true;
+    }
+
+    public void DisableMovement()
+    {
+        canMove = false;
+    }
+
+    public void DisableCamera()
+    {
+        cam.gameObject.SetActive(false);
+    }
+
+    public void EnableCamera()
+    {
+        cam.gameObject.SetActive(true);
+    }
+
 }
