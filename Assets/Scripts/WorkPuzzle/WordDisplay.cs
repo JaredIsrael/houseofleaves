31388434047575fd7dc6873f348acfa069ec4a3e
amandafleischer <fleischer.55@osu.edu>
2023-03-26 22:37:12 -0400

    text speed now based on screen size

--- Assets/Scripts/WorkPuzzle/WordDisplay.cs
+++ Assets/Scripts/WorkPuzzle/WordDisplay.cs
@@ -9,7 +9,7 @@ public class WordDisplay : MonoBehaviour
 {
     public TextMeshProUGUI text;
     //speed in which the text will fall down the screen
-    private float speed = 50f;
+    private float speed = .15f * Screen.height;
 
     //list of words on the screen
     public GameObject[] words;
