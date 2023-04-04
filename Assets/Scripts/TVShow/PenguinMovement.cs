96c7a679eb1f30730c832a74a71dbadcbf67eba7
amandafleischer <fleischer.55@osu.edu>
2023-04-04 17:08:00 -0400

    penguin walking animations

---
+++ Assets/Scripts/TVShow/PenguinMovement.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PenguinMovement : MonoBehaviour
+{
+    public Animator animator;
+    public SpriteRenderer spRenderer;
+
+    // Update is called once per frame
+    void Update()
+    {
+        animator.SetFloat("Horizontal", Input.GetAxis("Horizontal"));
+
+        Vector3 horizontal = new Vector3(Input.GetAxis("Horizontal"), 0.0f, 0.0f);
+
+        if (animator.GetCurrentAnimatorStateInfo(0).IsName("penguin_walkleft"))
+        {
+            spRenderer.flipX = true;
+        } else
+        {
+            spRenderer.flipX = false;
+        }
+
+        transform.position += horizontal * Time.deltaTime;
+
+    }
+}
