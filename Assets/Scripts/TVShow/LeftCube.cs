ed0644eefcf62e7fa6146201f37db636d57a3304
amandafleischer <fleischer.55@osu.edu>
2023-04-05 15:45:52 -0400

    starting to test out the use of speech bubbles

---
+++ Assets/Scripts/TVShow/LeftCube.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LeftCube : MonoBehaviour
+{
+    //Informs player that they can not move past this point
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        Debug.Log("Turn Around!");
+    }
+}
