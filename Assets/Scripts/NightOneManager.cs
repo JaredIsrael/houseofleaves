83c0eea97084b42b203d7c0ea6deb89a56c422cf
Cadecicc <cadeciccone@gmail.com>
2023-04-10 15:57:33 -0400

    NightOneAlmostFinal
    Okay, it's done (?)
    If anything needs changed or modified please let me know!

---
+++ Assets/Scripts/NightOneManager.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NightOneManager : MonoBehaviour
+{
+    [SerializeField] AudioSource[] audioSources;
+    private int index;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        index = 0;
+        StartCoroutine(StartAmbientMusic());
+    }
+
+    private IEnumerator StartAmbientMusic()
+    {
+        double offset = 0.01;
+        double length = audioSources[0].clip.length;
+        double time = AudioSettings.dspTime - offset;
+        audioSources[index].Play();
+
+        while (true)
+        {
+            yield return new WaitForSecondsRealtime((float)length);
+            time += length;
+            index = (index + 1) % audioSources.Length;
+            audioSources[index].PlayScheduled(time);
+        }
+    }
+}
