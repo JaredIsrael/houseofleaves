d1fdeb66cad800b211ff59133d5a6d9ad187bfb6
amandafleischer <fleischer.55@osu.edu>
2023-04-17 02:23:03 -0400

    complete idle, walk, jump animations

--- Assets/Scripts/Platformer/PlayerMovement.cs
+++ Assets/Scripts/Platformer/PlayerMovement.cs
@@ -5,22 +5,53 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     [SerializeField] private Rigidbody2D rigidBody;
+    [SerializeField] private SpriteRenderer spRenderer;
+    [SerializeField] private Animator animator;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    private float horizontal;
+
+    private enum AnimationState { idle, walk, jump, slide }
 
     // Update is called once per frame
     void Update()
     {
-        float horizontal = Input.GetAxisRaw("Horizontal");
+        horizontal = Input.GetAxisRaw("Horizontal");
         rigidBody.velocity = new Vector2(horizontal * 7f, rigidBody.velocity.y);
 
         if (Input.GetKeyDown(KeyCode.Return))
         {
             rigidBody.velocity = new Vector2(rigidBody.velocity.x, 14f);
         }
+
+        UpdateAnimationState();
+    }
+
+    private void UpdateAnimationState()
+    {
+        AnimationState state;
+
+        //walk state
+        if (horizontal > 0f)
+        {
+            state = AnimationState.walk;
+            spRenderer.flipX = false;
+        }
+        else if (horizontal < 0f)
+        {
+            state = AnimationState.walk;
+            spRenderer.flipX = true;
+        }
+        else
+        {
+            state = AnimationState.idle;
+        }
+
+        //jump state
+        if (rigidBody.velocity.y > 0.1f)
+        {
+            state = AnimationState.jump;
+        }
+
+        animator.SetInteger("State", ((int)state));
     }
 }
