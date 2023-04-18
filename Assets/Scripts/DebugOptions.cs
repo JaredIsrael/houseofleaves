16edbc6ef956b7c04c818a180663a69b98ce1f0b
Cadecicc <cadeciccone@gmail.com>
2023-04-18 16:28:59 -0400

    Menu Scene Select Options
    Currently have it set up to switch to scene selection screen and load the scenes in (just have day one and night one)

---
+++ Assets/Scripts/DebugOptions.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class DebugOptions : MonoBehaviour
+{
+    public void DayOneLoad()
+    {
+        SceneManager.LoadSceneAsync("Day1House");
+    }
+
+    public void NightOneLoad()
+    {
+        SceneManager.LoadSceneAsync("NightOneScene");
+    }
+}
