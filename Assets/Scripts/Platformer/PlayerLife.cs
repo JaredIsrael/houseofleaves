ab47adc3a8d771852f23963734e8f180aaca7531
amandafleischer <fleischer.55@osu.edu>
2023-04-17 23:25:07 -0400

    death animation and level restart

---
+++ Assets/Scripts/Platformer/PlayerLife.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlayerLife : MonoBehaviour
+{
+    [SerializeField] private Animator animator;
+    [SerializeField] private Rigidbody2D rigidBody;
+
+    private void Start()
+    {
+        
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Trap"))
+        {
+            PlayerDie();
+        }
+    }
+
+    //method disables player movement and plays death animation
+    private void PlayerDie()
+    {
+        rigidBody.bodyType = RigidbodyType2D.Static;
+        animator.SetTrigger("Death");
+    }
+
+    //method restarts the scene (level) after dying
+    private void RestartLevel()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+}
