557a8edaac88095ee9c69666d54480f0f0f0e203
amandafleischer <fleischer.55@osu.edu>
2023-04-24 17:28:59 -0400

    fixed some slide bugs

--- Assets/Scripts/Platformer/PlayerMovement.cs
+++ Assets/Scripts/Platformer/PlayerMovement.cs
@@ -46,8 +46,9 @@ public class PlayerMovement : MonoBehaviour
         {
             sliding = true;
         }
-        else if (sliding && Physics2D.Raycast(transform.position, Vector2.up, 1, layer))
+        else if (sliding && Physics2D.Raycast(transform.position, Vector2.up, 2, layer))
         { //raycast - if there is an object above the player, they can't exit slide
+            Debug.Log("hitting");
             sliding = true;
         }
         else
