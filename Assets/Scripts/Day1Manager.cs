ac83dc0b189f212d9ef160296fac01c0dd14f8c5
Jared Israel <jared.seattle@gmail.com>
2023-04-28 18:38:38 -0400

    Bug fixes for build

--- Assets/Scripts/Day1Manager.cs
+++ Assets/Scripts/Day1Manager.cs
@@ -29,6 +29,7 @@ public class Day1Manager: MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        Cursor.visible = false;
         StartCoroutine(FadeInFromBlackIntroDialog());
         ObjectivesManager.AllTasksCompletedEvent.AddListener(EnableSleep);
     }
