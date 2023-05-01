f692f5e2ff87cc40a1dc904403ee41ba183bf50c
amandafleischer <fleischer.55@osu.edu>
2023-05-01 00:25:26 -0400

    just some spelling corrections and fit work passages to fit context

--- Assets/Scripts/WorkPuzzle/PassageGenerator.cs
+++ Assets/Scripts/WorkPuzzle/PassageGenerator.cs
@@ -28,19 +28,20 @@ public class PassageGenerator : MonoBehaviour
         "that", "day..." };
 
     private static string[] passage2 = { "Alright", "day", "two...", "It", "feels",
-        "like", "this", "house", "just", "keeps", "getting", "smaller.", "Maybe", "I",
+        "like", "this", "house", "got", "smaller", "over", "night.", "Maybe", "I",
         "am", "going", "crazy.", "To", "be", "fair,", "I", "didn't", "get", "much",
         "sleep", "last", "night.", "Hopefully", "tonight", "is", "better." };
 
-    private static string[] passage3 = { "Keys", "keys", "keys!", "What", "is", "the",
-        "big", "deal?", "Ugh", "can", "not", "even", "focus", "on", "work", "anymore.",
-        "That", "door...", "do", "I", "really", "want", "to", "find", "the", "key...",
-        "do", "I", "even", "want", "to", "go", "in?" };
+    private static string[] passage3 = { "What", "is", "the", "big", "deal", "about",
+        "this", "key?", "Ugh", "can", "not", "even", "focus", "on", "work", "anymore.",
+        "Do", "I", "even", "want", "to", "find", "the", "key...", "do", "I", "even", "want",
+        "to", "go", "in?", "I", "do", "miss", "her...", "want", "to", "remember..."};
 
     private static string[] passage4 = { "Maybe", "I", "should", "have", "taken", "more",
-        "time", "off.", "That", "dream", "was", "the", "worst", "one", "yet.", "But", "again",
-        "with", "these", "keys!", "I", "can't", "go", "in", "that", "room...", "I", "shouldn't...",
-        "I", "think", "calling", "in", "sick", "tomorrow", "would", "be", "best." };
+        "time", "off.", "I", "think", "calling", "in", "sick", "tomorrow", "would", "be", "for",
+        "the", "best.", "That", "dream", "was", "the", "worst", "one", "yet.", "But", "now",
+        "I", "have", "the", "key!", "No...", "I", "can't", "go", "in", "her", "room...", "I",
+        "shouldn't..." };
 
     private static string[][] passageList = { passage1, passage2, passage3, passage4 };
 
